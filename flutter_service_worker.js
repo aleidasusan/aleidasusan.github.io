e5206e52f6bef340a7251428c6e0ad6c731ec086
esteban miño <minoesteban1@gmail.com>
2022-07-10 20:43:45 +0200

    New version

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "023a2612cf0a924224239466ab10c742",
-"index.html": "1a545dd558fbcdd0e937592c6abc43bd",
-"/": "1a545dd558fbcdd0e937592c6abc43bd",
-"main.dart.js": "684030ba5f4b84e4c71560647102143a",
+"index.html": "709bff99b608d173255d8cf40229d063",
+"/": "709bff99b608d173255d8cf40229d063",
+"main.dart.js": "c82edb68e2f90c97e177df714cd48548",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
